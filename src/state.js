580ce39b9e208e0cfc67a4d87436f48ed73fa93d
CyberSphinxxx <johnlemargonzales@gmail.com>
2026-09-06 17:13:05 +0800

    feat(state): create state management class to track bot mode and targets

---
+++ src/state.js
@@ -0,0 +1,41 @@
+const { Vec3 } = require('vec3');
+
+class BotState {
+  constructor(config) {
+    this.reset(config);
+  }
+
+  reset(config = {}) {
+    this.currentState = 'ROAM'; // ROAM, FOLLOW, GUARD, FARM, FISHING, COMBAT, SLEEPING, IDLE, DEPOSITING
+    this.homePos = config.home ? new Vec3(config.home.x, config.home.y, config.home.z) : null;
+    this.chestPos = config.chest ? new Vec3(config.chest.x, config.chest.y, config.chest.z) : null;
+    this.followTarget = null;
+    this.combatTarget = null;
+    this.isFarming = false;
+    this.isFishing = false;
+    this.isSleeping = false;
+    this.isDepositing = false;
+    this.playerShiftHistory = {};
+    this.isReplyingShift = false;
+  }
+
+  setState(newState) {
+    this.currentState = newState;
+  }
+
+  getState() {
+    return this.currentState;
+  }
+
+  setHome(vec) {
+    this.homePos = vec ? new Vec3(vec.x, vec.y, vec.z) : null;
+  }
+
+  setChest(vec) {
+    this.chestPos = vec ? new Vec3(vec.x, vec.y, vec.z) : null;
+  }
+}
+
+module.exports = {
+  BotState
+};
